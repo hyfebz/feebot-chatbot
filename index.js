fe359095a931cf1c968d1cdba3a999c9b5db9983
Febri <ardiyansafebri@gmail.com>
2025-08-05 00:49:28 +0700

    Add view front-end

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import { GoogleGenAI } from "@google/genai";
 import express from "express";
 import multer from "multer";
 import fs from "fs/promises";
-import { json } from "stream/consumers";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from 'cors';
 
 const extractGeneratedText = (data) => {
   try {
@@ -24,18 +26,41 @@ const app = express();
 // declare variable untuk multer
 const upload = multer();
 
+// buat 2 variable ajaib (magic variable)
+// __
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // declare variable default-nya
 const DEFAULT_GEMINI_MODEL = "gemini-2.5-flash";
 const DEFAULT_PORT = 3000;
+
+// utility variable untuk membuat satu object mapper
+const modelMapper = {
+  'flash': 'gemini-2.5-flash',
+  'flash-lite': 'gemini-2.5-flash-lite',
+  'pro': 'gemini-2.5-pro',
+};
+
+// helper function untuk menentukan model gemini
+const determineGeminiModel = (key) => {
+  return modelMapper[key] ?? DEFAULT_GEMINI_MODEL;
+};
 
 // instantiation --> memanggil class menjadi sebuah instance
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
 
+// memanggil middleware untuk bisa handle CORS
+app.use(cors());
+
 // memanggil middleware untuk bisa terima header
 // dengan Content-Type: application/json
 app.use(express.json());
+
+// panggil middleware untuk serve static file
+app.use(express.static(path.join(__dirname, 'public')));
 
 // tambah routing untuk handle model-nya
 // app.post('/', (req, res) => {})
@@ -60,6 +85,49 @@ app.post("/generate-text", async (req, res) => {
     res.json({ result: extractGeneratedText(aiResponse) });
   } catch (err) {
     res.status(500).json({ message: err.message });
+  }
+});
+
+app.post("/chat", async (req, res) => {
+  try {
+    if (!req.body) {
+      // perlu ada payload yang dikirim
+      // HTTP 400 --> Bad Request
+      //      ^   --> user yang melakukan "kesalahan"
+      //       ^^ --> 404 --> Not Found (nggak ketemu halaman/item-nya)
+      //          --> 403 --> Forbidden (akses tidak dibolehkan)
+      //          --> 401 --> Unauthorized (belum login)
+      return res.json(400, "Invalid request body!");
+    }
+
+    // extract messages dari request body
+    const { messages } = req.body;
+
+    // cek messages-nya
+    if (!messages) {
+      // kirim kalau nggak ada message-nya
+      return res.json(400, "Pesannya masih kosong nih!");
+    }
+
+    const payload = messages.map((msg) => {
+      return {
+        role: msg.role,
+        parts: [{ text: msg.content }],
+      };
+    });
+
+    const aiResponse = await ai.models.generateContent({
+      model: determineGeminiModel('pro'),
+      contents: payload,
+      config: {
+        systemInstruction:
+          "Anda adalah seorang asisten AI yang bertugas membantu pengguna dalam memberikan informasi dan bantuan.",
+      },
+    });
+
+    res.json({ reply: extractGeneratedText(aiResponse) });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
   }
 });
 
@@ -141,31 +209,31 @@ app.post(
 // Endpoint untuk menghasilkan teks dari audio
 app.post("/generate-from-audio", upload.single("audio"), async (req, res) => {
   try {
-          const prompt = req.body?.prompt;
+    const prompt = req.body?.prompt;
 
-      // guard clause 1
-      if (!prompt) {
-        res.status(400).json({ message: "Belum ada prompt yang diisi!" });
-        return;
-      }
+    // guard clause 1
+    if (!prompt) {
+      res.status(400).json({ message: "Belum ada prompt yang diisi!" });
+      return;
+    }
 
-      const file = req.file;
+    const file = req.file;
 
-      // guard clause
-      if (!file) {
-        res.status(400).json({ message: "File document harus di-upload!" });
-        return;
-      }
+    // guard clause
+    if (!file) {
+      res.status(400).json({ message: "File document harus di-upload!" });
+      return;
+    }
 
-      const audioBase64 = req.file.buffer.toString('base64');
-      const aiResponse = await ai.models.generateContent({
-        model: DEFAULT_GEMINI_MODEL,
-        contents: [
-            { text: prompt},
-            { inlineData: { mimeType: req.file.mimetype, data: audioBase64 } }
-        ]
-      });
-      res.json({ result: extractGeneratedText(aiResponse) });
+    const audioBase64 = req.file.buffer.toString("base64");
+    const aiResponse = await ai.models.generateContent({
+      model: DEFAULT_GEMINI_MODEL,
+      contents: [
+        { text: prompt },
+        { inlineData: { mimeType: req.file.mimetype, data: audioBase64 } },
+      ],
+    });
+    res.json({ result: extractGeneratedText(aiResponse) });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
